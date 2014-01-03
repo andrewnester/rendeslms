689d02635ef0b8f679dba752bde66ec865227a3a
andrewnester <newaltgroup@bk.ru>
2014-01-03 14:39:47 +0300

    added logging module and mongoose(module to access mongodb)

--- lrs/server/app.js
+++ lrs/server/app.js
@@ -1,13 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
+
+var log = require('./libs/log')(module);
+var mongoose = require('./libs/mongoose');
+var config = require('./libs/config');
 
 var auth = require('http-auth');
 var basic = auth.basic({
@@ -18,7 +17,7 @@ var basic = auth.basic({
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', config.get('port'));
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
 
@@ -42,5 +41,5 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  log.info("Express server listening on port " + app.get('port'));
 });
---
+++ lrs/server/libs/mongoose.js
@@ -0,0 +1,15 @@
+var mongoose    = require('mongoose');
+var config      = require('./config');
+var log         = require('./log')(module);
+
+mongoose.connect(config.get('mongoose:uri'));
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    log.error('connection error:', err.message);
+});
+db.once('open', function callback () {
+    log.info("Connected to DB!");
+});
+
+module.exports = mongoose;
